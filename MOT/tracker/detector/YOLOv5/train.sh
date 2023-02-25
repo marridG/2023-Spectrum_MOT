37e90db3424938b91b93168c64722f0dd5f98d82
marridG <marridG@sjtu.edu.cn>
2023-02-26 01:52:33 +0800

    fetch server train logs & res & updates

--- MOT/tracker/detector/YOLOv5/train.sh
+++ MOT/tracker/detector/YOLOv5/train.sh
@@ -4,4 +4,7 @@
 python train.py --img 640 --batch 16 --epochs 3 --data data_custom/medium_0/data.yaml --weights yolov5s.pt
 
 python train.py --img 1280 --batch 32 --epochs 50 --data data_custom/medium_0/data.yaml --weights yolov5s.pt --device 0,1
-nohup python train.py --img 1280 --batch 32 --epochs 50 --data data_custom/medium_0/data.yaml --weights yolov5s.pt --device 0,1 > nohup.txt 2>&1 &
+# yolov5s - PID=
+nohup python train.py --img 1280 --batch 32 --epochs 300 --data data_custom/medium_0/data.yaml --weights yolov5s.pt --device 0,1 > nohup.txt 2>&1 &
+# yolov5n - PID=24076
+nohup python train.py --img 1280 --batch 32 --epochs 300 --data data_custom/medium_0/data.yaml --weights yolov5n.pt --device 1,2 > nohup.txt 2>&1 &
